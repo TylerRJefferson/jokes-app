3d02e9d00b3159c213a4d14dd4ade54a5199d535
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-08 15:52:38 -0500

    logic for app setup

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,22 +6,37 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   // create state to hold jokes
+  const [jokeList, setJokeList] = useState([]);
   // create state to hold last joke index
+  const [currentJokeIndex, setCurrentJokeIndex] = useState(0);
   // create function to get another
+  const getNextJoke = () => {
+    if(currentJokeIndex , jokeList.length - 1){
+      setCurrentJokeIndex(currentJokeIndex + 1)
+    } else {
+      setCurrentJokeIndex(0)
+    }
+  }
   // useEffect to get all jokes
+  useEffect(() => {
+    fetch("https://api.sampleapis.com/jokes/goodJokes")
+      .then(result => result.json())
+      .then(setJokeList)
+      .catch(alert)
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>🤡🤡🤡🤡🤡🤡🤡</Text>
+      <Text>🤡🤣🤡🤣🤡🤣🤡🤣🤡🤣🤡🤣🤡</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffcc00',
     alignItems: 'center',
     justifyContent: 'center',
   },
