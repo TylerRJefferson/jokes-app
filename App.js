bb53c2bcd1e7f04938ac3c171ec27a00b467daac
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-08 15:35:03 -0500

    framework

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import Joke, Punchline, and Another components
+
 export default function App() {
+  // create state to hold jokes
+  // create state to hold last joke index
+  // create function to get another
+  // useEffect to get all jokes
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>🤡🤡🤡🤡🤡🤡🤡</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
     backgroundColor: '#fff',
